3a6d81510d381dfc7fe48075eac5dbbc54809a01
zhenizhui <zhuinizhe@gmail.com>
2016-11-09 23:50:04 +0800

    Fill the pit

--- js-prototype/js-prototype-1.js
+++ js-prototype/js-prototype-1.js
@@ -12,5 +12,10 @@ A.prototype = {
 var a = new A;
 var b = new A;
 
-console.log("a.aa === b.aa ==> "+ a.aa === b.aa);
-console.log("a.method === b.method ==> "+a.method === b.method);
+console.log( a.aa === b.aa);//true
+console.log(a.method === b.method);//true
+
+/**
+ * 定义在原型上的方法和属性为所有实例共享
+ * 
+ */
